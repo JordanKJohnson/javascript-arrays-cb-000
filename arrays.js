49c86abedb53fa55d78f29fc59bac10e89cb189a
Jordan Johnson <jkj86@hotmail.com>
2017-09-12 15:22:17 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,33 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  array.unshift(element);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+}
+
+function addElementToEndOfArray(array, element){
+  array.push(element);
+}
+
+function accessElementInArray(array, element){
+  Console.log(array[element]);
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.shift(element);
+}
+
+function removeElementFromEndOfArray(array){
+  array.slice(0, array.length - 1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array[array.length - 1] = undefined;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift();
+}
