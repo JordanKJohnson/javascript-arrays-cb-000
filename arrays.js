5f0b310bd71cf214f986ad1191e5858166c54db6
Jordan Johnson <jkj86@hotmail.com>
2017-09-12 16:01:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
 function addElementToBeginningOfArray(a, element){
-  array = [...a,element];
+  array = [element,...a];
   return array;
 }
 
